0fad466ac099989d52cd0afa48e24c7e7d8e334b
Klaus <bhavankrithik33@gmail.com>
2026-04-07 01:13:32 +0530

    Delete databaseconfig.py

--- databaseconfig.py
+++
@@ -1,6 +0,0 @@
-# database_config.py
-# DO NOT share this publicly if you have sensitive data!
-FIREBASE_CONFIG = {
-    "databaseURL": "https://agni-95975-default-rtdb.firebaseio.com/",
-    "secret": "AmNgspAhmU5tjIgYQU2voQdW0q9dN64bh9V5fps5"
-}
